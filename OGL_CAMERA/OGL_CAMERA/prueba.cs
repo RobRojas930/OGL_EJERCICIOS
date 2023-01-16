fef32e3733403506268fc6f1ae9400db96864da8
RobFibotech <103949750+RobFibotech@users.noreply.github.com>
2023-01-15 21:56:35 -0600

    init

---
+++ OGL_CAMERA/OGL_CAMERA/prueba.cs
@@ -0,0 +1,6 @@
+enum TipoPizza
+{
+    Chica = 1,
+    Mediana = 2,
+    Grande =3
+}
---
+++ OGL_LIGHTING/OGL_COLORS/light.fs
@@ -0,0 +1,7 @@
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0); // Solo le pasamos el color de la luz todo en 1 osea blanco
+}
